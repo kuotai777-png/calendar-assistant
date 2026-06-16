967bee5b2dec6c85a77b513ad732fb231db8af75
kuotai777-png <kuotai777@gmail.com>
2026-06-16 11:50:51 +0800

    Update parser.js

--- utils/parser.js
+++ utils/parser.js
@@ -1,10 +1,8 @@
 export function parseSchedule(text) {
-
   let date = "今天";
   let time = "未指定";
   let title = text;
 
-  // 日期
   if (text.includes("明天")) {
     date = "明天";
     title = title.replace("明天", "");
@@ -15,48 +13,35 @@ export function parseSchedule(text) {
     title = title.replace("後天", "");
   }
 
+  if (text.includes("今天")) {
+    date = "今天";
+    title = title.replace("今天", "");
+  }
 
-  // 下午時間
-  let afternoon =
-    text.match(/下午(\d+)點/);
+  const afternoon = text.match(/下午(\d+)點/);
 
   if (afternoon) {
-
-    let hour =
-      Number(afternoon[1]) + 12;
-
-    time =
-      hour + ":00";
-
-    title =
-      title.replace(
-        afternoon[0],
-        ""
-      );
+    const hour = Number(afternoon[1]) + 12;
+    time = hour + ":00";
+    title = title.replace(afternoon[0], "");
   }
 
-
-  // 上午時間
-  let morning =
-    text.match(/上午(\d+)點/);
+  const morning = text.match(/上午(\d+)點/);
 
   if (morning) {
-
-    time =
-      morning[1] + ":00";
-
-    title =
-      title.replace(
-        morning[0],
-        ""
-      );
+    time = morning[1] + ":00";
+    title = title.replace(morning[0], "");
   }
 
+  title = title.trim();
+
+  if (!title) {
+    title = "未命名行程";
+  }
 
   return {
     date,
     time,
-    title: title.trim()
+    title
   };
-
 }
