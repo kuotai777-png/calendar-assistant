0b084a13dd7a2ed4533397556d53680e6eb79ed5
kuotai777-png <kuotai777@gmail.com>
2026-06-18 16:28:48 +0800

    Update parser.js

--- utils/parser.js
+++ utils/parser.js
@@ -399,5 +399,19 @@ function formatDisplay(date){
     `${String(date.getHours()).padStart(2,"0")}:` +
     `${String(date.getMinutes()).padStart(2,"0")}`
   );
+  function formatDateOnly(date){
+
+  const y = date.getFullYear();
+
+  const m = String(
+    date.getMonth() + 1
+  ).padStart(2,"0");
+
+  const d = String(
+    date.getDate()
+  ).padStart(2,"0");
+
+  return `${y}-${m}-${d}`;
+}
 
 }
